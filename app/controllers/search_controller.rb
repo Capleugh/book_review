5f9a470c9da8837375d437699c8d6a0a504e73f6
Carleigh Crockett <capleugh@gmail.com>
2020-02-21 10:46:55 -0700

    Render view successfully

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,18 +4,19 @@ class SearchController < ApplicationController
       f.adapter Faraday.default_adapter
     end
 
-    book_response = book_conn.get("search.json?title=the_man_who_saw_everything")
+    book_response = book_conn.get("search.json?title=the_man_who_saw_everything&author=deborah_levy")
 
     review_conn = Faraday.new(url: "https://api.nytimes.com/svc/books/v3") do |f|
       f.adapter Faraday.default_adapter
     end
 
-    review_response = review_conn.get("reviews.json?title=The+Man+Who+Saw+Everything?author=&#{ENV['NYTIMES_API_KEY']}")
+    review_response = review_conn.get("reviews.json?title=The+Man+Who+Saw+Everything&api-key=#{ENV["NYTIMES_API_KEY"]}")
 
     book_json = JSON.parse(book_response.body, symbolize_names: true)
     review_json = JSON.parse(review_response.body, symbolize_names: true)
 
-    @book = book_json[:docs]
+# require "pry"; binding.pry
+    @book = book_json[:docs][0]
     @reviews = review_json[:results]
   end
 end
